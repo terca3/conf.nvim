6fbef8b0576805ab60c64d67c96ba334c166fb18
mduvnjak <mduvnjak@protonmail.com>
2024-03-06 18:03:10 +0100

    Add colorscheme. Add filepicker to telescope.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -70,6 +70,9 @@ return {
           },
         },
       }
+      -- To get telescope-file-browser loaded and working with telescope,
+      -- you need to call load_extension, somewhere after setup function:
+      require("telescope").load_extension "file_browser"
 
       -- Enable telescope extensions, if they are installed
       pcall(require('telescope').load_extension, 'fzf')
